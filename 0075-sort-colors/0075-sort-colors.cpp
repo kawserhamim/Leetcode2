3cbb3f8ad9c959fc0de51b268b493d245920e519
kawser hamim <mdkawserhamim91824@gmail.com>
2025-09-28 01:30:46 +0600

    Time: 0 ms (100%), Space: 11.8 MB (11.92%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
-    void sort(vector<int> &v, int l, int r, int mid)
-    {
-        int i = l , j = mid + 1 ;
-        vector<int> hamim, ans ;
-        while(i <= mid and j <= r)
-        {
-          if(v[i] <= v[j])hamim.push_back(v[i++]);
-          else hamim.push_back(v[j++]);
-        }
-        while(i <= mid)hamim.push_back(v[i++]);
-        while(j <= r) hamim.push_back(v[j++]);
-        for(int k = l ; k <= r ; k++)
-        {
-            v[k] = hamim[k - l];
-        }
-    }
-    void merge(vector<int> &v, int l , int r)
-    {
-      if(l >= r) return ;
-      int mid = (l + r) / 2 ;
-      merge(v, l ,  mid);
-      merge(v, mid + 1 , r);
-
-      sort(v, l , r, mid); 
-    }
     void sortColors(vector<int>& nums) {
-    merge(nums, 0 , nums.size()-1 );    
+        int x = 0, y = 0, z = 0;
+        
+     
+        for(int num : nums) {
+            if(num == 0) x++;
+            else if(num == 1) y++;
+            else z++;
+        }
+        
+        int i = 0;
+        while(x--) nums[i++] = 0;
+        while(y--) nums[i++] = 1;
+        while(z--) nums[i++] = 2;
     }
 };
