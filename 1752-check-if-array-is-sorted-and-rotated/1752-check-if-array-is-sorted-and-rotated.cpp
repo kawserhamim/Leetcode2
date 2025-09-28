438c727b90002ca022042d4a502a766f824d2d28
kawser hamim <mdkawserhamim91824@gmail.com>
2025-09-28 16:59:12 +0600

    Time: 0 ms (100%), Space: 11.2 MB (75.75%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -5,19 +5,34 @@ public:
         int n = nums.size(), m = 0 ;
         for(int i = 0 ; i <  nums.size(); i++)
         {
-            if(nums[i] <= x)
+            if(nums[i] < x)
             {
                 x = nums[i];
                 m = i ;
             }
         }    
-        for(int i =  1 ; i < n ; i++)
+         for(int i = 0 ; i <  nums.size(); i++)
         {
-            if(nums[(i + m)%n]>=x)
-            {
-                x = nums[(i + m)%n];
-            }else return false ;
+           if(nums[i] ==  x)
+           {
+             int x = 0 , y = -1 ;
+             for(int j = i ; j < n ;j++)
+             {
+              if(y <= nums[j])
+              {
+                y = nums[j];
+              }else x = 1 ;
+             }
+             for(int j = 0 ; j < i ;j++)
+             {
+              if(y <= nums[j])
+              {
+                y = nums[j];
+              }else x = 1 ;
+           }
+           if(x == 0) return true ;
+        }  
         }
-        return true ;
+        return false ; 
     }
 };
